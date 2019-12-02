ed87637ad7ce59d87bdddc467710701c2c3697c4
Alejandro <aesbetancourt@outlook.com>
2019-12-02 17:33:49 -0400

    getting current day and filter plates

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,11 +9,13 @@
         </ion-header>
         <ion-content class="ion-content" padding>
             <ion-item>
-                <ion-label position="stacked">Placa <ion-text color="danger">*</ion-text></ion-label>
-                <ion-input placeholder="Ingrese placa" id="placa"></ion-input>
+<!--                <ion-label position="stacked">Placa <ion-text color="danger">*</ion-text></ion-label>-->
+<!--                <ion-input placeholder="Ingrese placa"></ion-input>-->
+                <label for="placa">Ingrese placa</label>
+                <input type="text" id="placa" v-model="id" placeholder="Ingrese placa" >
 
             </ion-item>
-            <ion-button @click="checkID">Verificar</ion-button>
+            <ion-button @click="checkPlateID">Verificar</ion-button>
         </ion-content>
     </ion-page>
 </template>
@@ -23,13 +25,68 @@
     // import db from '../database.js'
     // const dateTime = require('date-time');
 
+
+
+    function lastDigit(plate) {
+        let plat = [...plate].reverse();
+        for (let i = 0; i < plat.length; i++) {
+            var last_number = plat[i];
+            if (!isNaN(last_number)) {
+                break
+            }
+        }
+        return last_number
+    }
+
+
+    let current_day;
+    function askAPI() {
+        const request = new XMLHttpRequest();
+        request.open('GET', 'http://worldclockapi.com/api/json/est/now', true);
+        request.onload = function() {
+            let data = JSON.parse(this.response);
+            current_day = data["dayOfTheWeek"];
+        };
+        request.send()
+    }
+
+
+    // Monday: 0123, tuesday 456, wednesday 789, thursday 0123, friday 456, saturday 789
+    function isValidDay(plate) {
+        let digit = lastDigit(plate);
+        console.log(digit, current_day);
+        let valid_day;
+        if ((digit === 0 || digit === 1 || digit === 2 || digit === 3) && (current_day === "Monday" || current_day === "Thursday")){
+           valid_day = true
+        } else if ((digit === 4 || digit === 5 || digit === 6) && (current_day === "Tuesday" || current_day === "Friday")){
+            valid_day = true
+        } else if ((digit === 7 || digit === 8 || digit === 9) && (current_day === "Wednesday" || current_day === "Saturday")){
+            valid_day = true
+        } else {
+            valid_day = true
+        }
+        return valid_day
+    }
+
+
     export default {
         data(){
             return{
-                id: null
+                id: null,
+                day: null,
+                valid: false
             }
         },
         methods: {
+            checkPlateID() {
+                console.log(this.id);
+                console.log(isValidDay(this.id));
+
+
+                // if (this.id !== null){
+                //     this.throwModal()
+                // }
+            },
             throwModal(){
                 return this.$ionic.modalController
                     .create({
@@ -45,16 +102,10 @@
                     })
                     .then(m => m.present())
             },
-            checkID() {
-                console.log(this.id)
-                if (this.id !== null){
-                    this.throwModal()
-                }
-
-
-
-            },
         },
+        created() {
+            askAPI()
+        }
     }
 </script>
 <style scoped>
