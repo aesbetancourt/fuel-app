620408257eb0076104dd9734b6cb70dd2a31d2ba
Alejandro <aesbetancourt@outlook.com>
2019-12-02 12:47:11 -0400

    components

---
+++ src/components/Home.vue
@@ -0,0 +1,62 @@
+<template>
+    <ion-page class="ion-page" main>
+        <ion-header>
+            <ion-toolbar color="primary">
+                <ion-title>
+                    Sistema de gestión de surtido
+                </ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-content" padding>
+            <ion-item>
+                <ion-label position="stacked">Placa <ion-text color="danger">*</ion-text></ion-label>
+                <ion-input placeholder="Ingrese placa" id="placa"></ion-input>
+
+            </ion-item>
+            <ion-button @click="checkID">Verificar</ion-button>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+    import Modal from './Modal.vue'
+    // import db from '../database.js'
+    // const dateTime = require('date-time');
+
+    export default {
+        data(){
+            return{
+                id: null
+            }
+        },
+        methods: {
+            throwModal(){
+                return this.$ionic.modalController
+                    .create({
+                        component: Modal,
+                        componentProps: {
+                            data: {
+                                content: 'New Content',
+                            },
+                            propsData: {
+                                title: 'New title',
+                            },
+                        },
+                    })
+                    .then(m => m.present())
+            },
+            checkID() {
+                console.log(this.id)
+                if (this.id !== null){
+                    this.throwModal()
+                }
+
+
+
+            },
+        },
+    }
+</script>
+<style scoped>
+
+</style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <ion-header translucent>
+            <ion-toolbar>
+                <ion-title>Registro de Placas</ion-title>
+                <ion-buttons slot="end">
+                    <ion-button onclick="dismiss()">Volver</ion-button>
+                </ion-buttons>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+            <ion-item>
+                <ion-label position="stacked">Verifique placa antes de continuar</ion-label>
+                <ion-input type="text" readonly>1234567</ion-input>
+            </ion-item>
+            <ion-item>
+                <ion-label>Estación de servicio</ion-label>
+                <ion-select placeholder="Seleccionar">
+                    <ion-select-option value="1">Estacion 1</ion-select-option>
+                    <ion-select-option value="2">Estacion 2</ion-select-option>
+                    <ion-select-option value="3">Estacion 3</ion-select-option>
+                    <ion-select-option value="4">Estacion 4</ion-select-option>
+                    <ion-select-option value="5">Estacion 5</ion-select-option>
+                    <ion-select-option value="6">Estacion 6</ion-select-option>
+                </ion-select>
+            </ion-item>
+            <ion-button color="primary">Registrar</ion-button>
+        </ion-content>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Modal',
+        props: {
+            title: { type: String, default: 'Super Modal' },
+        },
+        data() {
+            return {
+                content: 'Content',
+            }
+        },
+        methods:{
+        }
+    }
+</script>
